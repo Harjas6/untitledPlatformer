85c93b84e22f40ade4d412b06f8e10e8aa04181f
Harjas <hbansoot@students.cs.ubc.ca>
2023-08-13 22:57:32 -0700

    put basic structure down to make multiple levels

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Game():
         self.screen = pygame.display.set_mode((WIDTH,HEIGHT),pygame.DOUBLEBUF)
         self.level = Level_1('level/tmx/untitledPlatformerTile1.tmx')
         self.clock = pygame.time.Clock()
+        self.level_num = 1
 
     def run(self):
         while True:
@@ -19,12 +20,17 @@ class Game():
                     pygame.quit()
                     sys.exit()
             self.screen.fill('blue')
-            if not self.level.game_over:
+
+            if self.level.next_level:
+                self.level = self.choose_level()
+
+            elif not self.level.game_over:
                 self.level.run()
                 self.clock.tick(FPS)
                 pygame.display.update()
+
             else:
-                game_over = pygame.image.load('images/games_over.png')
+                game_over = pygame.image.load('images/game_over.png')
                 self.screen.fill('blue')
                 self.screen.blit(game_over, (WIDTH/3, HEIGHT/3))
                 self.handle_keys()
@@ -37,6 +43,10 @@ class Game():
         elif keys[pygame.K_q]:
             pygame.quit()
             sys.exit()
+
+    def choose_level(self):
+        match self.level_num:
+            case 1: return Level_1('level/tmx/untitledPlatformerTile1.tmx')
 
 if __name__ == '__main__':
     game = Game()
